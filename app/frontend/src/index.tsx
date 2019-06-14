7681cf434ea20cb8bb216ab43b5c98f57ecd28ff
Nils Hartmann <nils@nilshartmann.net>
2019-06-14 14:15:31 +0200

    add build folder, make example app run from filesystem

--- app/frontend/src/index.tsx
+++ app/frontend/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { ApolloProvider } from "@apollo/react-hooks";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import createApolloClient from "./infra/createApolloClient";
 import NavigationProvider from "./infra/NavigationProvider";
